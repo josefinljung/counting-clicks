faa86a29585f1d8b3e63f8e7ebbc0d3cd105d9fc
josefinljung <58325534+josefinljung@users.noreply.github.com>
2021-10-27 13:07:38 +0200

    Connected repo to project, deleted unnecessary files and created the Counter component with buttons in it.

---
+++ src/components/Counter.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Counter() {
+
+    return (
+       <React.Fragment>
+
+        <button id="decrement-btn">- DECREMENT</button>
+        <button id="increment-btn">+ INCREMENT</button>
+
+
+
+
+       </React.Fragment>
+    );
+};
+    
+export default Counter;
